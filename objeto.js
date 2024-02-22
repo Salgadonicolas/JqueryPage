7a473cb0d677f2932c26e16f602ac43758495313
nicolas <nicolassalg@yahoo.com.br>
2024-02-22 08:33:42 -0300

    pagina jquery

---
+++ objeto.js
@@ -0,0 +1,18 @@
+{
+    "objeto": [{
+        "nome": "Andrius",
+        "sobrenome": "Salgado",
+        "idade": 35
+    },
+    {
+        "nome": "Nicolas",
+        "sobrenome": "Salgado",
+        "idade": 29
+    },
+    {
+        "nome": "Daiana",
+        "sobrenome": "Turino",
+        "idade": 30
+    }
+    ]
+}
